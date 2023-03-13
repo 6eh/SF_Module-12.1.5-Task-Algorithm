c90325a019d07a5770200077cc2f00e795480c41
6eh <mcls@list.ru>
2023-03-13 18:53:18 +0100

    Skill Factory Module 12 Algorithms
    Skill Factory Module 12 Algorithms
    Task 12.1.5

---
+++ SF_Module-12.1.5-Task-Algorithm/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace SF_Module_12._1._5_Task_Algorithm
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            User[] users = new User[]
+            {
+                new User() { Login = "leo", Name = "Leonardo", IsPremium = false },
+                new User() { Login = "mike", Name = "Mikelangelo", IsPremium = true },
+                new User() { Login = "don", Name = "Donatello", IsPremium = true },
+                new User() { Login = "raf", Name = "Rafael", IsPremium = false }                
+            };
+
+            foreach (var user in users)
+            {
+                Console.WriteLine($"Hi, {user.Name}!");
+
+                if (!user.IsPremium)
+                    ShowAds();
+                Console.WriteLine("---");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+    }
+}
+
+public class User
+{
+    public string Login { get; set; }
+    public string Name { get; set; }
+    public bool IsPremium { get; set; }
+}
